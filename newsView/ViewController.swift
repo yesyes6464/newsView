b3ac16b7238df9d12828a65a1af8ed9427b60b46
yesyes6464@naver.com <yesyes6464@naver.com>
2020-03-04 14:11:44 +0900

    Initial Commit

---
+++ newsView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsView
+//
+//  Created by 김지오 on 2020/03/04.
+//  Copyright © 2020 JiO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
